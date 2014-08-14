4db9c569cb8d94bda16ddf0cebcf2c456fe937c3
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-08-14 19:40:05 -0400

    Creates a basic project and converts it to swift.

---
+++ ChuckNorris/ChuckNorris/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChuckNorris
+//
+//  Created by Christopher Rittersdorf on 8/14/14.
+//  Copyright (c) 2014 Christopher Rittersdorf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
